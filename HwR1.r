ed54c618a99185fcf34a1cce70aa239a1f266895
sonamss <60361348+sonamss@users.noreply.github.com>
2020-02-03 17:36:17 -0500

    Update HwR1.r

--- HwR1.r
+++ HwR1.r
@@ -3,12 +3,15 @@ Sonam Serchan
 2017^(1/3)
 abs(5.7-6.8)/.58
 a = 1:12
+a
 b = c(1,3,5,7,9,11)
+b
 c = seq(1,11,2)
+c
 ln.a = log(a)
+ln.a
 b^2
 ?sd 
 The sd function computes/calculates the standard deviation of the values in x.
 Name = "Sonam"
 paste("my name is", Name)
-
