f1ed4a2f5d6d574f0a8e66e18523117dacffc3fd
Salma106 <60075252+Salma106@users.noreply.github.com>
2020-02-03 13:21:01 -0500

    Add files via upload

---
+++ Salma.r
@@ -0,0 +1,16 @@
+2017^(1/3)
+abs(5.7-6.8)/.58
+a = c(1:12)
+a
+b = c(1, 3, 5, 7, 9, 11)
+b
+c = seq(1,11, 2)
+c
+ln.a = log(a)
+ln.a
+b^2
+Name="Salma"
+paste("My name is," Name)
+sd: Description
+This function computes the standard deviation of the values in x. If na.rm is TRUE then missing values are removed before computation proceeds.
+Usage: sd(x, na.rm = FALSE)
