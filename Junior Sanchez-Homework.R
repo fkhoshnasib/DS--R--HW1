c23aa422480c4109a719166273230de5017ecaf5
junale26 <60361040+junale26@users.noreply.github.com>
2020-02-03 18:47:28 -0500

    Junior Sanchez-Homework 01

---
+++ Junior Sanchez-Homework.R
@@ -0,0 +1,14 @@
+2017^(1/3)
+abs(5.7-6.8)/.58
+a = 1:12
+a
+b = seq(1, 11, 2)
+b
+c = seq(11, 1, -2)
+c
+log(a)
+sqrt(b)
+?sd
+"This function computes the standard deviation of the values in x. If na.rm is TRUE then missing values are removed before computation proceeds."
+Name = "Junior"
+paste("My name is", Name)
