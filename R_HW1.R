7e44ce732eb57c1df271422c8b5640e2f787670f
ishres19 <60153931+ishres19@users.noreply.github.com>
2020-02-03 13:20:53 -0500

    Add files via upload

---
+++ R_HW1.R
@@ -0,0 +1,12 @@
+Irisa Shrestha
+2017^(1/3)
+abs(5.7-6.8)/.58
+a = 1:12
+b = c(1, 3, 5, 7, 9, 11)
+c = seq(1,11, 2)
+ln.a = log(a)
+c^2
+The sd function calculates the standard deviation. 
+Name= "irisa"
+paste("My name is", Name)
+
