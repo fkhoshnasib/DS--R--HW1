950716032c4fa459abea20b93ce1b2ed0d010358
ishres19 <60153931+ishres19@users.noreply.github.com>
2020-02-03 17:34:48 -0500

    Update R_HW1.R

--- R_HW1.R
+++ R_HW1.R
@@ -2,9 +2,13 @@ Irisa Shrestha
 2017^(1/3)
 abs(5.7-6.8)/.58
 a = 1:12
+a
 b = c(1, 3, 5, 7, 9, 11)
+b
 c = seq(1,11, 2)
+c
 ln.a = log(a)
+ln.a
 c^2
 The sd function calculates the standard deviation. 
 Name= "irisa"
