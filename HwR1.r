ea82ff241730d9653116cc685c912dc855b37485
sonamss <60361348+sonamss@users.noreply.github.com>
2020-02-03 13:22:44 -0500

    Add files via upload
    Homework1

---
+++ HwR1.r
@@ -0,0 +1,14 @@
+Sonam Serchan
+
+2017^(1/3)
+abs(5.7-6.8)/.58
+a = 1:12
+b = c(1,3,5,7,9,11)
+c = seq(1,11,2)
+ln.a = log(a)
+b^2
+?sd 
+The sd function computes/calculates the standard deviation of the values in x.
+Name = "Sonam"
+paste("my name is", Name)
+
